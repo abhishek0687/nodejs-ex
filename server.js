cdaa96b252d904419a80d542107ff36714008c40
abhishek <abhishek0687@gmail.com>
2018-01-25 18:05:11 +0530

    ajjjjjjj

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    //initDb(function(err){});
+    console.log("Error connecting db");
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
